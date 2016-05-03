b038f235d107e3e77f926cd1b6e4ab45e5f949fe
Jonathan Barket <jbarket@sleepunit.com>
2016-05-03 15:28:41 -0500

    First stab at blue apron scraping proxy

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+var url = require('url');
+
+app.get('*', function(req, res) {
+  // var parsedUrl = url.parse(req.url, true);
+  //
+  // parsedUrl.query.key = process.env.BREWERYDB_KEY;
+  // parsedUrl.search = null;
+  // req.url = url.format(parsedUrl);
+
+  request('https://www.blueapron.com' + req.url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.send(body);
+    }
+   });
+});
+
+app.listen(process.env.PORT || 8000);
